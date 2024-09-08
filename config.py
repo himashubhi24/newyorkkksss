563e55c64a7523bda0494630261d51ee0d3e4e93
rajputfc <95620124+himashubhi24@users.noreply.github.com>
2024-09-08 21:26:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002193779756"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}!\nPlease also Join our backup channel.")
+START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}!\nPlease also Join our backup @foxylinkk channel.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "5598929281 5353698695").split()):
@@ -53,10 +53,10 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group ")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in @foxylinkk my Channel/Group ")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We  or its subsidiary channel doesn't produce any of them.")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We  or its subsidiary @foxylinkk channel doesn't produce any of them.")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -65,7 +65,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "join @mahilalinks"
+USER_REPLY_TEXT = "join @foxylinkk"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6852649461)
