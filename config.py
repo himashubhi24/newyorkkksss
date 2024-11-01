9a6972d4300a8a89b5ab57a999e21819ceed91ed
rajputfc <95620124+himashubhi24@users.noreply.github.com>
2024-11-01 20:29:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = "mongodb+srv://hoodsitegraphics:PnrXiisxj73wI2vl@hoodsite96.kkrhjnb.mon
 DB_NAME = os.environ.get("DATABASE_NAME", "hoodsitegraphics")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "0fc7682dbc24201efce25076a06f34a05ef0ea14")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "81fcb04914c8839992c24f02e03d291012b1838f")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","t.me/tokenbotuse/6")
