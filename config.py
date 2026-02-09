f2f2613ce4bca5b58d7b2fb4ae71b4dc633ee612
rajputfc <95620124+himashubhi24@users.noreply.github.com>
2026-02-10 01:40:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002677894900"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}!\nPlease also Join our backup @mahilalinks channel.")
+START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}!\nPlease also Join our backup  channel.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6763725032").split()):
@@ -53,7 +53,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in @mahilalinks my Channel/Group ")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in  my Channel/Group ")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We  or its subsidiary @foxylinkk channel doesn't produce any of them.")
@@ -65,7 +65,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "join @mahilalinks"
+USER_REPLY_TEXT = "join "
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6810248021)
